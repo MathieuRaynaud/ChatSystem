56432a795f03166e3b4aad943589b7b6929ecae9
Raynaud <mraynaud@etud.insa-toulouse.fr>
2017-12-21 16:58:58 +0100

    Avancee dans la sauvegarde des conversations mais erreur pointeur null non résolue

--- src/main/java/com/fredericboisguerin/insa/chatSystem/FileManager.java
+++ src/main/java/com/fredericboisguerin/insa/chatSystem/FileManager.java
@@ -19,13 +19,9 @@ public class FileManager{
    public void saveConv(String utilisateur, ArrayList<StringTuple> conversation) {
        try {
            // Nous prenons comme convention que chaque utilisateur distant a un fichier nomme sonNom.txt dans le dossier data ou toute la conversation est sauvegarde
-           System.out.println("0");
            String pathname = new File("").getAbsolutePath();
-           System.out.println("1");
            File ConvFile = new File(pathname + utilisateur+".txt");
-           System.out.println("2");
            ConvFile.setWritable(true);
-           System.out.println("3");
 
            FileWriter stylo = new FileWriter(ConvFile, true);
            Iterator<StringTuple> it = conversation.iterator();
